e69b0dc0d1d86aec16b106ded96f7ad8e0a84e26
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-12-19 20:01:00 +0300

    add sequelize and global guard

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from "./users/users/users.module";
+import { ConfigModule } from "@nestjs/config";
+import { User } from "./users/users/users.model";
+import { RolesModule } from "./roles/roles.module";
+import { Role } from "./roles/roles.model";
+import { UserRoles } from "./roles/user-roles.model";
+import { AuthModule } from "./auth/auth.module";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+    }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User, Role, UserRoles],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+    RolesModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
