97806aee809309c9db08aa3e2ec43c3c503d5d7d
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-12-19 20:00:30 +0300

    add swagger and app listen

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { JwtService } from "@nestjs/jwt";
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle("Trade Company Application")
+    .setDescription("Документация REST API")
+    .setVersion("1.0.0")
+    .addTag("TRADECOMP")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
